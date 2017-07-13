b757f37fc95c35689139f73439f617b93c79a60c
Chandra Kellwan <Chandra@Chandras-MacBook-Pro.local>
2017-07-12 23:43:46 -0400

    going to bed

--- liri.js
+++ liri.js
@@ -8,89 +8,96 @@
 
 
 var liriKeys = require("./keys.js");
-var twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('require');
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var request = require('request');
 var fs = require('fs');
 
 
 
-var writeToLog = function(data) {
-  fs.appendFile("log.txt", '\r\n\r\n');
+function writeToLog (data) {
+  fs.appendFile("log.txt", '\n', {
 
-  fs.appendFile("log.txt", JSON.stringify(data), function(err) {
-    if (err) {
-      return console.log(err);
-    }
-
-    console.log("log.txt was updated!");
+  	'flag': 'a'
+  }, function(err) {
+  		if (err) {
+  			return console.error(err);
+  	}
+    
   });
-}	
+  	console.log(data);
+}
 
-var getArtistNames = function(artist) {
-  return artist.name;
+
+var spotify = new Spotify({
+    id: keys.spotifyKeys.client_id,
+    secret: keys.spotifyKeys.client_secret
+});
+
+var client = new Twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+function getMeSong(trackName) {
+    spotify.search({
+        type: 'track',
+        query: trackName,
+        limit: 1
+    }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+
+        writeToLog("Spotify search results: ");
+
+
+		var songs = data.tracks.items[0];
+		songs.album.artists.forEach(function(artist, index) {
+			writeToLog("Artist(s): \n" + (index + 1") " + artist.name);
+		})
+
+		logWriter("Album Name: " + songReturned.album.name);
+        logWriter("Song Name: " + songReturned.name);
+        logWriter("Preview URL: " + songReturned.preview_url);
+
+    })
+
 };
-
-var getMeSong = function(songName) {
-	if (songName === undefined) {
-		songName = 'The Sign - Ace of Base';
-
-	}
-};
-
-
-	spotify.search({ type: 'track', query: songName }, function(err, data) {
-		if (err) {
-			console.log( 'Error occured: ' + err);
-		}
-	
-	}
-
-		var songs = data.tracks.items;
-		var data = []; 
-
-		for (var i = 0; i < songs.length; i++) {
-			data.push({
-				'artist(s)': songs[i].artists.map(getArtistNames),
-				'song name: ': songs[i].name,
-				'preview song: ': songs[i].preview_url,
-				'album: ': songs[i].album.name,
-			});
-		}
+        
 		
-var getMeTweets = function() {
-			var client = new twitter(liriKeys.twitterKeys);
+		
+function getMeTweets() {
+			client.get('search/tweets', {
 
-			var params = { screen_name: 'mitum', count: 20 };
-			 client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			q: { 'mitum', 
+			count: 20 
+		}, function(error, tweets, response) {
 
-    			if (!error) {
-      				var data = []; 
-      				for (var i = 0; i < tweets.length; i++) {
-        			data.push({
-            			'created at: ' : tweets[i].created_at,
-            			'Tweets: ' : tweets[i].text,
-        });
-      }
-      console.log(data);
-      writeToLog(data);
-    }
-  });
-};
+    		writeToLog("My Tweets!");
+    		tweets.statuses.forEach(function(tweet, index)_{
+    			writeToLog((index + 1) + ") " + tweet.txt);
+    		
+  			})
 
+    		logWriter("Ending Tweets!");
 
-var getMeMovie = function(movieName) {
+	});
+}
 
-  if (movieName === undefined) {
-    movieName = 'Mr Nobody';
-  }
+function getMeMovie (data) {
 
-  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json";
+  request ("http://www.omdbapi.com/?t=" + data + '&apikey=40e9cece&tomatoes=true', function(error, response, body) {
 
-  request(urlHit, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = [];
-      var jsonData = JSON.parse(body);
+        if (error) {
+            console.log('error:', error);
+        } else {
+            var movieObject = JSON.parse(body);
+
+  
 
       data.push({
       'Title: ' : jsonData.Title,
@@ -111,7 +118,7 @@ var getMeMovie = function(movieName) {
 
 }
 
-var doWhatItSays = function() {
+ function doWhatItSays(data) {
   fs.readFile("random.txt", "utf8", function(error, data) {
     console.log(data);
     writeToLog(data);
@@ -141,7 +148,7 @@ var pick = function(caseData, functionData) {
       doWhatItSays();
       break;
     default:
-      console.log('LIRI doesn\'t know that');
+      console.log('LIRI didn\'t get that');
   }
 }
 
